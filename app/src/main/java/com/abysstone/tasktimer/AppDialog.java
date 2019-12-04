a55a729b551b8e53b3d84603022f8dcfe19d0f38
“abysstone” <manasbhatt007@gmail.com>
2019-12-04 14:28:02 +0530

    status bar material and app compatibility rework for 16 api dialog fragment

--- app/src/main/java/com/abysstone/tasktimer/AppDialog.java
+++ app/src/main/java/com/abysstone/tasktimer/AppDialog.java
@@ -1,6 +1,5 @@
 package com.abysstone.tasktimer;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,6 +8,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 public class AppDialog extends DialogFragment {
